4f887b669fa7012c2c6cca3f0235abdc2cc069a1
江茂琼 <jiangmaoqiong@lizhi.fm>
2021-08-30 11:16:41 +0800

    init

---
+++ src/components/Button/button.test.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Button, { ButtonProps } from './Button'
+
+const defaultProps = {
+  onClick: jest.fn()
+}
+
+const testProps: ButtonProps = {
+  btnType: 'primary',
+  size: 'lg',
+  className: 'klass'
+}
+
+const disabledProps: ButtonProps = {
+  disabled: true,
+  onClick: jest.fn(),
+}
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    //  wrapper 获取到通过render方法解析的React组件实例信息
+    const wrapper = render(<Button {...defaultProps}>Nice</Button>)
+    // element 则是通过 text 值获取到的类似“DOM”
+    const element = wrapper.getByText('Nice') as HTMLButtonElement
+    // 通过 JEST 框架可以做一系列断言
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default')
+    expect(element.disabled).toBeFalsy()
+    // 触发元素click事件
+    fireEvent.click(element)
+    // 断言模拟事件被触发
+    expect(defaultProps.onClick).toHaveBeenCalled()
+  })
+  it('should render the correct component based on different props', () => {
+    const wrapper = render(<Button {...testProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice')
+    expect(element).toBeInTheDocument()
+    expect(element).toHaveClass('btn-primary btn-lg klass')
+  })
+  it('should render a link when btnType equals link and href is provided', () => {
+    const wrapper = render(<Button btnType='link' href="http://dummyurl">Link</Button>)
+    const element = wrapper.getByText('Link')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('A')
+    expect(element).toHaveClass('btn btn-link')
+  })
+  it('should render disabled button when disabled set to true', () => {
+    const wrapper = render(<Button {...disabledProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice') as HTMLButtonElement
+    expect(element).toBeInTheDocument()
+    expect(element.disabled).toBeTruthy()
+    fireEvent.click(element)
+    expect(disabledProps.onClick).not.toHaveBeenCalled()
+  })
+})
